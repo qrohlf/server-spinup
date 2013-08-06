3110e4c5a405e4f66c2b9ac70ce35409d852bf96
Quinn Rohlf <QRohlf@gmail.com>
2013-08-06 10:42:56 -0700

    not tested at all

--- server-spinup.sh
+++ server-spinup.sh
@@ -15,18 +15,32 @@ PACKAGES="git make ruby1.9.1 nginx-full"
 # Comment this line out for no GUI
 GUI="xfce4 xubuntu-artwork xubuntu-default-settings" 
 
+# 0. Sanity Check
+###############################################
+
+# Make sure we  are being run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 
 # 1. Add the new user and grant root privileges
 ###############################################
+PASS=`(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)`
+adduser -p $PASS -g sudo --gecos "" $ADMINUSER
+echo user $ADMINUSER created with generated password $PASS
 
 # 2. Disallow root login via SSH
 ###############################################
+sed -i 's/^PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 
 # 3. Add PPA for passenger-nginx
 ###############################################
+apt-get update
+apt-get install python-software-properties # needed for apt-add-repository
+apt-add-repository ppa:brightbox/ruby-ng # includes nginx with passenger, newer ruby versions
 
-# 4. Install dev packages
+# 4. Install dev packages and GUI
 ###############################################
-
-# 5. Install GUI
-###############################################
+apt-get update
+apt-get install $PACKAGES $GUI
