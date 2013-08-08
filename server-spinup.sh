3ab1eb898daf0b72c64c1141d9c9c0d4fd6cb75a
Quinn Rohlf <QRohlf@gmail.com>
2013-08-08 08:32:17 -0700

    fix su installation of sexy-bash-prompt

--- server-spinup.sh
+++ server-spinup.sh
@@ -54,5 +54,5 @@ apt-get install -y $PACKAGES $GUI
 
 # 5. Install sexy-bash-prompt to $ADMINUSER and root bashrc
 ###############################################
-(cd /tmp && git clone --depth 1 https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install) && source ~/.bashrc
-su -c (cd /tmp/sexy-bash-prompt && make install) qrohlf
+cd /tmp && git clone --depth 1 https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install
+su -c "(cd /tmp/sexy-bash-prompt && make install)" qrohlf #doesn't seem to be working yet
